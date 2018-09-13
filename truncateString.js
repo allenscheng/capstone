2fb6d99735ba3a71e894325b5b406744957f9f63
Allen Cheng <allencheng219@gmail.com>
2018-09-12 22:15:30 -0500

    truncate a string

---
+++ truncateString.js
@@ -0,0 +1,4 @@
+function truncateString(str, num) {
+    return num >= str.length? str : str.slice(0, num) + '...';
+}
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
